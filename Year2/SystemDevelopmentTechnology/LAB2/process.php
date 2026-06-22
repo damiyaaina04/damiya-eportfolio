5bf202d017b2dc7b3460bb741513225289bfc010
Damiya Aina Basir Abd Shammad <damiyaaina@graduate.utm.my>
2026-06-22 23:30:55 +0800

    Add process code

---
+++ Year2/SystemDevelopmentTechnology/LAB2/process.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['name'])){
+ 
+    $studentid = $_POST['student_id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $program = $_POST['program'];
+    $gender = $_POST['gender'];
+     
+}
+
+
+
+?>
